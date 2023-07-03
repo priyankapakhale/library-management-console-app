b373057e55ddbff9faed597f939498112c8e5e56
Priyanka Pakhale <priyanka.pakhale54@gmail.com>
2023-07-03 14:31:39 +0530

    added test cases for returning books in the library

--- library.test.js
+++ library.test.js
@@ -87,15 +87,71 @@ describe("borrow a book from the library", () => {
 })
 
 describe("return books to the library", () => {
-    it("when user has 2 books in borrowed list and returns 1 book, the book is removed from the borrowed list", () => { })
+    it("when user has 2 books in borrowed list and returns 1 book, the book is removed from the borrowed list", () => { 
+        let borrowedList = [1, 2]
+        const library = new Library(data);
 
-    it("when user has 2 books in borrowed list and returns 1 book, the book's quantity gets increased by 1 in the library", () => { })
+        library.returnBook(1, borrowedList);
+        
+        expect(borrowedList).not.toContain(1);
+        expect(borrowedList).toHaveLength(1);
+    })
 
-    it("when user has 2 books in borrowed list and returns both books, both books' quantity gets increased by 1 in the library", () => { })
+    it("when user has 2 books in borrowed list and returns 1 book, the book's quantity gets increased by 1 in the library", () => { 
+        let borrowedList = [1, 2]
+        const library = new Library(data);
 
-    it("when user has 2 books in borrowed list and returns both books, borrowed list becomes empty", () => { })
+        const previousQuantity = library.findBook(1).quantity;
+        library.returnBook(1, borrowedList);
+        
+        const newQuantity = library.findBook(1).quantity;
+        expect(newQuantity).toEqual(previousQuantity + 1);
+    })
 
-    it("when user has 1 book in borrowed list and returns it, borrowed list becomes empty", () => { })
+    it("when user has 2 books in borrowed list and returns both books, both books' quantity gets increased by 1 in the library", () => {
+        let borrowedList = [1, 2]
+        const library = new Library(data);
 
-    it("when user has 1 book in borrowed list and returns it, book's quantity gets increased by 1 in the library", () => { })
+        const previousQuantity1 = library.findBook(1).quantity;
+        const previousQuantity2 = library.findBook(2).quantity;
+
+        library.returnBook(1, borrowedList);
+        library.returnBook(2, borrowedList);
+
+        const newQuantity1 = library.findBook(1).quantity;
+        const newQuantity2 = library.findBook(2).quantity;
+
+        expect(newQuantity1).toEqual(previousQuantity1 + 1);
+        expect(newQuantity2).toEqual(previousQuantity2 + 1);
+     })
+
+    it("when user has 2 books in borrowed list and returns both books, borrowed list becomes empty", () => {
+        let borrowedList = [1, 2]
+        const library = new Library(data);
+
+        library.returnBook(1, borrowedList);
+        library.returnBook(2, borrowedList);
+        
+        expect(borrowedList).toHaveLength(0);
+     })
+
+    it("when user has 1 book in borrowed list and returns it, borrowed list becomes empty", () => { 
+        let borrowedList = [1]
+        const library = new Library(data);
+
+        library.returnBook(1, borrowedList);
+        
+        expect(borrowedList).toHaveLength(0);
+    })
+
+    it("when user has 1 book in borrowed list and returns it, book's quantity gets increased by 1 in the library", () => { 
+        let borrowedList = [1]
+        const library = new Library(data);
+
+        const previousQuantity = library.findBook(1).quantity;
+        library.returnBook(1, borrowedList);
+        
+        const newQuantity = library.findBook(1).quantity;
+        expect(newQuantity).toEqual(previousQuantity + 1);
+    })
 })
